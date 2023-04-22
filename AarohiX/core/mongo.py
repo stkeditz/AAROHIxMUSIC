16518674b53351ce3ff334f1754311ef712f5b93
Sagar Tiwari <112709751+stkeditz@users.noreply.github.com>
2023-04-22 09:56:40 +0530

    Add files via upload

---
+++ AarohiX/core/mongo.py
@@ -0,0 +1,10 @@
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from pymongo import MongoClient
+
+from config import MONGO_DB_URI
+
+
+_mongo_async_ = _mongo_client_(MONGO_DB_URI)
+_mongo_sync_ = MongoClient(MONGO_DB_URI)
+mongodb = _mongo_async_.Aarohi
+pymongodb = _mongo_sync_.Aarohi
