73e8d794d461e4f979497bfe5e5840670865224a
Sagar Tiwari <112709751+stkeditz@users.noreply.github.com>
2023-04-22 09:54:38 +0530

    Add files via upload

---
+++ AarohiX/__init__.py
@@ -0,0 +1,41 @@
+from AarohiX.core.bot import AarohiXBot
+from AarohiX.core.dir import dirr
+from AarohiX.core.git import git
+from AarohiX.core.userbot import Userbot
+from AarohiX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = AarohiXBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
