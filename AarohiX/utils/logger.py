c8fae8cb6c57e63d61e9e6a15b42fa20d5c0f9a5
Sagar Tiwari <112709751+stkeditz@users.noreply.github.com>
2023-04-22 10:00:11 +0530

    Add files via upload

---
+++ AarohiX/utils/logger.py
@@ -0,0 +1,37 @@
+from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
+from AarohiX import app
+from AarohiX.utils.database import is_on_off
+
+
+async def play_logs(message, streamtype):
+    if await is_on_off(LOG):
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+        logger_text = f"""
+**{MUSIC_BOT_NAME} ᴩʟᴀʏ ʟᴏɢɢᴇʀ**
+
+**✨ᴄʜᴀᴛ✨:** {message.chat.title} [`{message.chat.id}`]
+»»————- ★ - ★ ————-««
+**🥀ᴜsᴇʀ🥀:** {message.from_user.mention}
+»»————- ★ - ★ ————-««
+**🥰ᴜsᴇʀɴᴀᴍᴇ🥰:** @{message.from_user.username}
+»»————- ★ - ★ ————-««
+**🌹ɪᴅ🌹:** `{message.from_user.id}`
+»»————- ★ - ★ ————-««
+**🍃ᴄʜᴀᴛ ʟɪɴᴋ🍃:** {chatusername}
+
+**🕊️sᴇᴀʀᴄʜᴇᴅ ғᴏʀ🕊️:** {message.text}
+
+**🔻sᴛʀᴇᴀᴍ ᴛʏᴩᴇ🔻:** {streamtype}"""
+        if message.chat.id != LOG_GROUP_ID:
+            try:
+                await app.send_message(
+                    LOG_GROUP_ID,
+                    f"{logger_text}",
+                    disable_web_page_preview=True,
+                )
+            except:
+                pass
+        return
